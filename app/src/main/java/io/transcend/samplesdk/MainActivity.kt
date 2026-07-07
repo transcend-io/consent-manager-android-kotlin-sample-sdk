aefefcf1515aa4ed9e57d5e8efee754cbb95cdf5
Cesar Sosa <cesarsosa.p@gmail.com>
2026-07-07 16:57:55 +0200

    remove google services

--- app/src/main/java/io/transcend/samplesdk/MainActivity.kt
+++ app/src/main/java/io/transcend/samplesdk/MainActivity.kt
@@ -16,7 +16,6 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import io.transcend.webview.TranscendAPI
 import io.transcend.webview.TranscendWebView
 import io.transcend.webview.models.TranscendConfig
-//import com.google.android.gms.ads.MobileAds
 import io.transcend.webview.models.ConsentStatus
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
